5fe92178e06103afb7e9d4f7e1e730339f54b281
Milclimule <99533207+miclimule@users.noreply.github.com>
2023-05-17 01:52:00 +0300

    Initial commit

---
+++ src/main/java/com/mic/limule/BackPointVenteApplication.java
@@ -0,0 +1,13 @@
+package com.mic.limule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPointVenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPointVenteApplication.class, args);
+	}
+
+}
